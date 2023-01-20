f976570f38f9e99eaeadd19c49b6c8cf36d82761
Tori Franklin <toripfranklin@gmail.com>
2023-01-19 21:04:02 -0500

    Simon Says

---
+++ main.js
@@ -0,0 +1,12 @@
+const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
+
+for (const location of locations) {
+  let invalidLocation = false
+    if (location[0] > 2) {
+        invalidLocation = true
+    }
+
+    if (invalidLocation) {
+        console.log("This location is invalid")
+    }
+}
